b8e38a76b5539072919dcda04c470ec87a47d272
Kevin Roh <Schrws@users.noreply.github.com>
2019-10-14 15:31:27 +0900

    Fix Typo

--- app/src/main/java/io/schrws/cherrysms/android/di/Api.kt
+++ app/src/main/java/io/schrws/cherrysms/android/di/Api.kt
@@ -2,11 +2,11 @@ package io.schrws.cherrysms.android.di
 
 import com.google.gson.JsonObject
 import io.reactivex.Single
-import okhttp3.RequestBody
+import okhttp3.ResponseBody
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface Api {
     @POST("/default/CherrySMS")
-    fun postMessage(@Body body: JsonObject): Single<RequestBody>
+    fun postMessage(@Body body: JsonObject): Single<ResponseBody>
 }
